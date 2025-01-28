60502ac715f02a60f764667d398db5ff332851ad
SoumenCodes <soumen19j@gmail.com>
2025-01-29 00:59:18 +0530

    cards

--- app/forums/page.jsx
+++ app/forums/page.jsx
@@ -107,7 +107,7 @@ export default function ForumsPage() {
           </motion.p>
         </header>
         <motion.div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-20"
           initial="hidden"
           animate={isVisible ? "visible" : "hidden"}
           variants={{
